be70220054b9d725fb4edc17d167e058c55c1aa3
LincolnFellipe <lincolnfellipe171@gmail.com>
2022-12-17 14:50:10 -0300

    Implementando_Padrao_Option

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,9 +1,11 @@
+using DevFreela.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<OpeningTimeOption>(builder.Configuration.GetSection("OpeningTime"));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
