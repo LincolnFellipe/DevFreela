a0474fcfc80c8ac6015aafd4c8fa4b959278467a
LincolnFellipe <lincolnfellipe171@gmail.com>
2023-02-28 19:40:47 -0300

    Aplicacao_Dapper_Alguns_Metodos

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,11 +1,15 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +19,11 @@ namespace DevFreela.Application.Services.Implementations
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
-        public ProjectService(DevFreelaDbContext dbContext)
+        private readonly string _connectionString;
+        public ProjectService(DevFreelaDbContext dbContext,IConfiguration configuration)
         {
             _dbContext= dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
         public int Create(NewProjectInputModel inputModel)
         {
@@ -80,7 +86,15 @@ namespace DevFreela.Application.Services.Implementations
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Start();
-            _dbContext.SaveChanges();
+            //Comentei o saveChanges pois troquei esse metodo para uma execução do dapper
+            //_dbContext.SaveChanges();
+            //Implementação da query utilizando Dapper
+            using (var sqlConnection = new SqlConnection(_connectionString)) 
+            { 
+            sqlConnection.Open();
+                var script = "UPDATE Projects SET Status = @status, StartedAt = @startedat, WHERE Id = @id";
+                sqlConnection.Execute(script, new { status = project.Status, startedat = project.StartedAt, id }); // Aqui eu passei os parâmetros como um objeto, mas da pra utilizar coleções pra fazer isso tb.
+            }
         }
 
         public void Update(UpdateProjectInputModel inputModel)
