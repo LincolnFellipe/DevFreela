3dd9249ae4167a8f3d2043554231845af54deb34
LincolnFellipe <lincolnfellipe171@gmail.com>
2023-03-06 19:54:14 -0300

    Aplicando_Padrao_Repository_GetAllSkills

--- DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,11 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
     public class ProjectRepository : IProjectRepository
     {
         private readonly DevFreelaDbContext _dbContext;
-        public ProjectRepository(DevFreelaDbContext dbContext) 
+        private readonly string _connectionString;
+        public ProjectRepository(DevFreelaDbContext dbContext, IConfiguration configuration) 
         { 
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
         public async Task<List<Project>> GetAll() 
         { 
